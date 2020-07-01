7d143fd5bc23a7bb36fbaea946ea74ecac72b438
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2020-07-01 14:04:29 -0300

    add brew installation

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 echo updating ~/.vimrc file
 ln -s vim/.vimrc ~/.vimrc
+
+echo
+echo installing brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
