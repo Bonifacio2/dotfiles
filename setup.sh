5ef1ba1f66a4ec7fff982aa664b1adaff5841523
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2020-07-01 14:14:04 -0300

    add oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ ln -s vim/.vimrc ~/.vimrc
 echo
 echo installing brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+echo
+echo installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
